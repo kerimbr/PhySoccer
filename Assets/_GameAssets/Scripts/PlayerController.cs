289c8f20ddb118855d50dd77ee75810133f6e905
kerimbora <kerim.bora@appcent.mobi>
2025-02-21 17:49:12 +0300

    Add jump functionality

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -3,23 +3,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     private Rigidbody _playerRigidbody;
 
     [SerializeField] private Transform orientationTransform;
     
-    private float _verticalInput, _horizontalInput;
-    
+
+    private float
+        _verticalInput,
+        _horizontalInput;
+
+
     private Vector3 _moveDirection;
-    
-    
-    [Header("Movement")]
-    [SerializeField] private float moveSpeed = 10f;
+
+    [Header("Movement Settings")] [SerializeField]
+    private float moveSpeed = 10f;
+
+    [Header("Jump Settings")] [SerializeField]
+    private KeyCode jumpKey;
+
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool canJump;
+    [SerializeField] private float jumpCooldown;
+
+    [Header("Ground Check Settings")] [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField] private float playerHeight;
 
     private void Awake()
     {
         _playerRigidbody = GetComponent<Rigidbody>();
         _playerRigidbody.freezeRotation = true;
+        canJump = true;
     }
 
     private void Update()
@@ -36,13 +51,44 @@ public class PlayerController : MonoBehaviour
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
+        Debug.Log("canJump is" + canJump);
+        Debug.Log("Isgrounded is" + IsGrounded());
+        if (Input.GetKey(jumpKey) && canJump && IsGrounded())
+        {
+            canJump = false;
+            SetPlayerJump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
-    
+
+    private void SetPlayerJump()
+    {
+        _playerRigidbody.linearVelocity =
+            new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+        _playerRigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
     private void SetPlayerMovement()
     {
         _moveDirection = orientationTransform.forward * _verticalInput + orientationTransform.right * _horizontalInput;
-        _playerRigidbody.AddForce(_moveDirection.normalized * 10f, ForceMode.Force);
+        _playerRigidbody.AddForce(_moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
-    
-    
+
+    private void ResetJump()
+    {
+        canJump = true;
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay( new Vector3(transform.position.x, transform.position.y + playerHeight, transform.position.z),
+            Vector3.down, Color.red);
+        // detect is player grounded
+        return Physics.Raycast(
+             new Vector3(transform.position.x, transform.position.y + playerHeight, transform.position.z),
+            Vector3.down,
+            playerHeight,
+            groundLayer
+            );
+    }
 }
